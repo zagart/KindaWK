c8d4ff633b90fa8ce71c0f75dcd8e7966f25cb8b
wanderer005 <wanderer005@gmail.com>
2019-02-25 10:47:20 +0300

    Add Webview to main activity for future login to VK

--- app/src/main/java/com/vvsemir/kindawk/MainActivity.java
+++ app/src/main/java/com/vvsemir/kindawk/MainActivity.java
@@ -2,6 +2,7 @@ package com.vvsemir.kindawk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView wbWebView = (WebView)findViewById(R.id.webViewAuth);
+        wbWebView.getSettings().setJavaScriptEnabled(true);
+        wbWebView.clearCache(true);
     }
 }
